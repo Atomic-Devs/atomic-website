1c787d2c3e7fdfb4987050ad211f25054d4558f6
Chandni Patel <chandnip6@gmail.com>
2021-04-13 23:37:15 -0400

    Add scroolToTop feature
    Co-authored-by: Stanley Huang <stanleyhuang16@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import NavBar from '../src/Components/NavBar';
-import Footer from '../src/Components/Footer'
-import Header from '../src/Components/Header'
-import About from '../src/Components/About'
-import Features from '../src/Components/Features'
-import Demo from '../src/Components/Demo'
-import Team from '../src/Components/Team'
+import NavBar from "../src/Components/NavBar";
+import Footer from "../src/Components/Footer";
+import Header from "../src/Components/Header";
+import About from "../src/Components/About";
+import Features from "../src/Components/Features";
+import Demo from "../src/Components/Demo";
+import Team from "../src/Components/Team";
+import ScrollToTop from "../src/Components/ScrollToTop";
+import Fab from "@material-ui/core/Fab";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 
 function App() {
   return (
     <>
       <NavBar />
       <Header />
+
       <main>
         {/* <About /> */}
         <Features />
         {/* <Demo /> */}
         <Team />
       </main>
+      <ScrollToTop>
+        <Fab color="primary" size="small" aria-label="scroll to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollToTop>
       <Footer />
     </>
   );
